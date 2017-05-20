d1373ccf2ca790242fed0d11341954581aee5741
Sylvain Zimmer <sylvain@sylvainzimmer.com>
2017-05-20 11:51:54 +0200

    add package name

--- setup.py
+++ setup.py
@@ -4,26 +4,29 @@ import os
 
 if os.path.isfile("mmh3object.cpp"):
 
-    setup(
-        ext_modules=[
-            Extension(
-                "mmh3object",
-                ["mmh3object.cpp", "MurmurHash3.cpp"]
-            )
-        ]
-    )
+    ext_modules = [
+        Extension(
+            "mmh3object",
+            ["mmh3object.cpp", "MurmurHash3.cpp"]
+        )
+    ]
 
 else:
 
     from Cython.Build import cythonize
 
-    setup(
-        ext_modules=cythonize([
-            Extension(
-                "mmh3object",
-                ["mmh3object.pyx", "MurmurHash3.cpp"],
-                include_dirs=[os.path.abspath("./")],
-                language="c++"
-            )
-        ])
-    )
+    ext_modules = cythonize([
+        Extension(
+            "mmh3object",
+            ["mmh3object.pyx", "MurmurHash3.cpp"],
+            include_dirs=[os.path.abspath("./")],
+            language="c++"
+        )
+    ])
+
+
+setup(
+    name="mmh3object",
+    version="0.1",
+    ext_modules=ext_modules
+)
